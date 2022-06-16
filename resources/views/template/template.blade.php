ccd2384e6144f5417b2c2967123be86d23cac330
wushawn1027 <wushawn1027@gmail.com>
2022-06-16 15:04:45 +0800

    6/16-7

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -310,10 +310,10 @@
     {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> --}}
     <script src="{{ asset('js/hamburgerMenu.js') }}"></script>
 
-    <script>
+    {{-- <script>
         var check_account = {!!json_encode($)!!}
         console.log();
-    </script>
+    </script> --}}
     @yield('js')
 </body>
 
