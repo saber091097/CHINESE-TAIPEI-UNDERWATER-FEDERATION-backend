a9363b4c8b8b1a39c396a76b9d596969d5ae66bc
MilaZhao <67618679+MilaZhao@users.noreply.github.com>
2022-06-29 20:27:46 +0800

    about us

--- resources/views/about_us/index.blade.php
+++ resources/views/about_us/index.blade.php
@@ -72,7 +72,12 @@
 
         @media only screen and (max-width:840px) {
             #page {
-                margin: 24px auto 0 auto;
+                margin: 80px auto 0 auto;
+            }
+        }
+        @media only screen and (max-width:640px) {
+            #page {
+                margin: 100px auto 0 auto;
             }
         }
 
@@ -710,12 +715,12 @@
                 <span class="test" id="aboutspan">About</span>
             </div>
 
-            <div class="try">
-                <div class="asd">
+            <div class="waveAnimations">
+                {{-- <div class="asd">
                     <div id="imgsection">
                         <img id="banner_img" src="{{ asset('img/img_aboutUsBg1.png') }}" alt="">
                     </div>
-                </div>
+                </div> --}}
 
                 <!---------------- wave ---------------->
                 <section>
@@ -747,28 +752,7 @@
                     </svg>
 
                 </section>
-                <!---------------- wave black ---------------->
-                {{-- <div class="banner">
-                    <div class="wave-container">
 
-                        <div>
-                            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-                                <defs>
-                                    <path id="gentle-wave" stroke-width="1px" fill="none" stroke="rgb(255, 255, 255)"
-                                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                                </defs>
-                                <g class="parallax">
-                                    <use xlink:href="#gentle-wave" x="48" y="0" />
-                                    <use xlink:href="#gentle-wave" x="48" y="3" />
-                                    <use xlink:href="#gentle-wave" x="48" y="7" />
-                                    <use xlink:href="#gentle-wave" x="48" y="5" />
-                                </g>
-                            </svg>
-                        </div>
-
-                    </div>
-                </div> --}}
             </div>
 
         </div>
